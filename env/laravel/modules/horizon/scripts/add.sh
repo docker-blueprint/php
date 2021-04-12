11ec03cbb2cead05e04f9fc6bdfda98061104f2e
Aleksei Ivanov <contact@aleksei.dev>
2021-04-12 17:06:22 +0300

    Update horizon module add script

--- env/laravel/modules/horizon/scripts/add.sh
+++ env/laravel/modules/horizon/scripts/add.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-composer require laravel/horizon --no-scripts
+version_suffix=""
+
+if [[ -f composer.json ]]; then
+    existing_version=$(
+        cat composer.json |
+            grep 'laravel/horizon' | cut -d':' -f2 | sed -E 's/.*"(.*?)".*/\1/'
+    )
+
+    if [[ -n "$existing_version" ]]; then
+        version_suffix=":$existing_version"
+    fi
+fi
+
+composer require laravel/horizon$version_suffix --no-scripts
